0251375711df8d436efaf134c512cd49529730cf
LeBeurre <francis.forest121@gmail.com>
2018-12-13 16:56:23 -0500

    added comments

--- src/components/sections/evaluation.buttons.js
+++ src/components/sections/evaluation.buttons.js
@@ -3,10 +3,12 @@ import { withRouter } from "react-router-dom";
 import EvaluationAPI from "../../api/evaluation";
 import ResultStates from "../../api/ResultStates";
 
+// Contient les boutons pour évaluer un athlète sur un exercice
 class EvaluationButtons extends Component {
     constructor(props) {
         super(props);
 
+        // Variables des boutons
         this.state = {
             error: null,
             isLoaded: false,
@@ -17,12 +19,14 @@ class EvaluationButtons extends Component {
             state: this.props.evaluation.result_state
         }
 
+        // Méthodes des boutons
         this.addSuccess = this.addSuccess.bind(this);
         this.addTries = this.addTries.bind(this);
         this.isOver = this.isOver.bind(this);
         this.isSuccess = this.isSuccess.bind(this);
     }
 
+    // Ajoute un succès
     addSuccess = async function() {
         if (this.state.state !== ResultStates.TODO) {
             return;
@@ -36,6 +40,7 @@ class EvaluationButtons extends Component {
         this.addTries()
     } 
 
+    // Ajoute un essai
     addTries = async function() {
         if (this.state.state !== ResultStates.TODO) {
             return;
@@ -47,8 +52,10 @@ class EvaluationButtons extends Component {
             tries: newTries
         });
 
+        // Update la DB
         await EvaluationAPI.update(this.state.evaluation.id, {value: this.state.success});
 
+        // Vérifie si c'est le dernier essai avant la fin de l'exercice
         if (this.isOver()) {
             let newState = null;
             
@@ -62,25 +69,29 @@ class EvaluationButtons extends Component {
                 state: newState      
             }) 
 
+            // Update la DB
             await EvaluationAPI.update(this.state.evaluation.id, {value: this.state.success, state: newState});
         }
     }
 
+    // Vérifie si l'exercice est terminé
     isOver() {
         return this.state.tries >= this.state.drill.allowed_tries;
     }
 
+    // Vérifie si c'est un succès
     isSuccess() {
         return this.state.success >= this.state.drill.success_treshold;
     }
 
+    // Affichage des boutons
     render() {
         return (
             <div>
-                <span id='success'>Success : {this.state.success}</span>
+                <span id='success'>Succès : {this.state.success}</span>
                 <span id='total'>Total : {this.state.drill.allowed_tries}</span>
-                <button className='ui button' onClick={this.addSuccess}>Succ&egrave;s</button>
-                <button className='ui button' onClick={this.addTries}>Rat&eacute;</button>
+                <button className='ui button' onClick={this.addSuccess}>Succès</button>
+                <button className='ui button' onClick={this.addTries}>Raté</button>
             </div>
         )
     }
